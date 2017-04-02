58e3016d223c89edf7b1304e4eaee4cdf829bfaf
Anwar <sadique.anwar@gmail.com>
2017-04-01 19:42:39 -0500

    Updated to use JSON payload

--- src/main/java/com/example/ProducerApplication.java
+++ src/main/java/com/example/ProducerApplication.java
@@ -1,20 +1,18 @@
 package com.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.context.annotation.*;
+import org.springframework.messaging.*;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
 
 
 @SpringBootApplication
@@ -37,13 +35,34 @@ public class ProducerApplication {
         }
 
 
-        @GetMapping(value = "/producer/{message}")
-        public String sendMessage(@PathVariable("message") String message) {
-            System.out.println("Hello message is received " + message);
-            messageBinder.sendMessage(message);
+        @GetMapping(value = "/producer/{firstName}/{lastName}")
+        public String sendMessage(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) throws JsonProcessingException {
+            System.out.println("Sending message for : " + firstName + " " + lastName);
+            Person person = new Person(firstName, lastName);
+            messageBinder.sendMessage(person);
             return "Message sent successfully";
         }
 
+    }
+
+    static class Person implements Serializable {
+        static final long serialVersionUID = 42L;
+
+        private String firstName;
+        private String lastName;
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
     }
 
     @EnableBinding(MessageProducerChannel.class)
@@ -58,19 +77,21 @@ public class ProducerApplication {
 
         @Bean
         @Primary
-        public MessageBinder messageBinder(MessageProducerChannel channel){
+        public MessageBinder messageBinder(MessageProducerChannel channel) {
             return new MessageBinder(channel);
         }
 
-        public void sendMessage(String message){
-            Message<String> msg = MessageBuilder.withPayload(message).build();
+        public void sendMessage(Object message) throws JsonProcessingException {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Message<String> msg = MessageBuilder.withPayload(objectMapper.writeValueAsString(message))
+                    .setHeader(MessageHeaders.CONTENT_TYPE, "application/json").build();
             channel.output().send(msg);
         }
     }
 
     interface MessageProducerChannel {
 
-        @Output
+        @Output("foo")
         MessageChannel output();
     }
 }
